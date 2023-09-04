910ca2e9961464857273495f93bf251a3a79f6f1
kJAHsin <kjahnsencollins@gmail.com>
2023-09-04 22:51:45 +0200

    image fetch set up

--- js/fetch.js
+++ js/fetch.js
@@ -1,35 +1,34 @@
+// import { fetchImage } from "./fetchimage.js";
+
 const baseAPI = "https://blog.skinnyk.no/wp-json/wp/v2/";
 const fetchURL = `${baseAPI}posts`;
-const fetchImgURL = `${baseAPI}media`;
+// const fetchImgURL = `${baseAPI}media/`;
 
 export const fetchPosts = async () => {
     try {
         const results = await fetch(fetchURL);
         const posts = await results.json();
         const blogList = [];
+        
 
         posts.forEach(postInfo => {
             const postTitle = postInfo.title.rendered;
             const postContent = postInfo.content.rendered;
             const imageID = postInfo.featured_media;
-            // const imageRef = `${fetchImgURL}/${imageID}/`;
+            // const imageRef = fetchImage(fetchImgURL, imageID);
 
             const post = {
                 postid: postInfo.id,
                 title: postTitle,
                 content: postContent,
                 imgid: imageID,
-                // imgRef: imageRef,
-                // imgAlt: `${fetchImgURL}/${imageID}`
             };
+            
             blogList.push(post);
         })
-
         return blogList;
     }
     catch (error) {
         console.error(`%cDownload error: %c${error.message}`, "color: pink", "color: black; font-weight: bold;");
     }
 }
-
-fetchPosts();
